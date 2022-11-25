6627a79fb0659ec06b0a0cb915a6556811f071fb
Adrian Senkevich <adriansenkevich@Adrians-MacBook-Air.local>
2022-11-25 13:04:51 +0100

    Added homepage and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./Components/Header/header";
+import Home from "./Components/Homepage/home";
+import Footer from "./Components/Footer/footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
