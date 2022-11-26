c8dabec7ca56ae4f71c30445fff4101f54309ae5
Adrian Senkevich <adriansenkevich@Adrians-MacBook-Air.local>
2022-11-26 21:39:26 +0100

    Added elements to footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,9 +1,28 @@
 import React from "react"
 import "../Footer/footer.scss"
+import logo from '../Header/YourHealth.png'
+
 function Footer() {
     return (
         <footer className="footer">
-
+            <div className="footer__logo">
+                <div className="footer__img">
+                    <img src={logo}></img>
+                </div>
+                <div className="footer__brand">
+                    Your<br/>
+                    <span>Health</span>
+                </div>
+            </div>
+            <div className="footer__links">
+                <div className="footer__instagram"></div>
+                <div className="footer__facebook"></div>
+                <div className="footer__Email"></div>
+            </div>
+            <div className="footer__contact">
+                <div className="footer__phone"></div>
+                <div className="footer__adress"></div>
+            </div>
         </footer>
     )
 }
