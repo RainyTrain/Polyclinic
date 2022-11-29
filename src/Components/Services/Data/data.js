02b286b67ca1ebaf9c40d535d9437275a11239f2
Adrian Senkevich <adriansenkevich@Adrians-MacBook-Air.local>
2022-11-29 23:42:08 +0100

    Created data and modified service component

---
+++ src/Components/Services/Data/data.js
@@ -0,0 +1,92 @@
+export const data = [
+    {
+        name: 'aaa',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'dentist'
+    },
+    {
+        name: 'bbb',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'dentist'
+    },
+    {
+        name: 'aaa',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'allergist'
+    },
+    {
+        name: 'bbb',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'allergist'
+    },
+    {
+        name: 'ccc',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'allergist'
+    },
+    {
+        name: 'aaa',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'dermatologist'
+    },
+    {
+        name: 'bbb',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'dermatologist'
+    },
+    {
+        name: 'ccc',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'dermatologist'
+    },
+    {
+        name: 'aaa',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'gynecologist'
+    },
+    {
+        name: 'bbb',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'gynecologist'
+    },
+    {
+        name: 'ccc',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'gynecologist'
+    },
+    {
+        name: 'aaa',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'infectiologist'
+    },
+    {
+        name: 'bbb',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'infectiologist'
+    },
+    {
+        name: 'ccc',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'infectiologist'
+    },
+    {
+        name: 'ccc',
+        sername: 'bbbbbb',
+        age: 40,
+        specialisation: 'dentist',
+    }
+]
--- src/Components/Services/services.js
+++ src/Components/Services/services.js
@@ -1,11 +1,44 @@
-import React from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import "../Services/services.scss"
+import {data} from "./Data/data.js"
 
 function Services() {
+
+    const [service, setService] = useState('')
+    const [list, setList] = useState(data)
+
+
+    console.log(service)
+
+    const sortedBySpecialisation = useMemo(() => {
+        return [...list].filter(item => item.specialisation == service)
+    },[service])
+
+    console.log(sortedBySpecialisation)
+
+    //console.log(sortedBySpecialisation)
+
     return (
-        <div className="services">
-           Privet :)
-        </div>
+        <section className="services">
+            <div className="services__menu">
+                <div className="services__name">
+                    Choose service:
+                </div>
+                <div className="services__option">
+                    <select onChange={e => setService(e.target.value)}>
+                        <option value="">Please choose the service</option>
+                        <option value="dermatologist">Dermatologist</option>
+                        <option value="allergist">Allergist</option>
+                    </select>
+                </div>
+            </div>
+            <div className="services__element">
+                Service - {service}
+                {sortedBySpecialisation.map(item => {
+                    return <div key={item.name}>{item.name} {item.sername}</div>
+                })}
+            </div>
+        </section>
     )
 }
 
